6f6cb42967bf8fed65bce24c16f0f90472efbba5
James Halliday <mail@substack.net>
2014-10-22 18:43:40 +0200

    basic thing using wikidb

---
+++ bin/cmd.js
@@ -0,0 +1,56 @@
+#!/usr/bin/env node
+
+var fs = require('fs');
+var path = require('path');
+var mkdirp = require('mkdirp');
+var minimist = require('minimist');
+var defined = require('defined');
+
+var argv = minimist(process.argv.slice(2), {
+    alias: { h: 'help', d: 'datadir' }
+});
+
+if (argv.help || argv._[0] === 'help') {
+    usage(0);
+}
+if (argv.showdir) {
+    console.log(getdir());
+}
+else if (argv._[0] === 'edit') {
+    
+}
+else if (argv._[0] === 'search') {
+    var wdb = getwdb();
+    wdb.search(argv._.slice(1)).on('data', console.log);
+}
+else usage(1);
+
+function usage (code) {
+    var r = fs.createReadStream(path.join(__dirname, 'usage.txt'));
+    r.pipe(process.stdout);
+    r.on('end', function () {
+        if (code) process.exit(code);
+    });
+}
+
+function getwdb () {
+    var wikidb = require('wikidb');
+    var level = require('level');
+    
+    var dir = getdir();
+    var blobdir = path.join(dir, 'blob');
+    mkdirp.sync(blobdir);
+    
+    var db = level(path.join(dir, 'db'));
+    return wikidb(db, { dir: blobdir });
+}
+
+function getdir () {
+    var dir = defined(argv.datadir, process.env.HOWTO_PATH);
+    if (!dir) {
+        dir = defined(process.env.HOME, process.env.USERDIR);
+        if (dir) dir = path.join(dir, '.config', 'howto');
+    }
+    if (!dir) dir = process.cwd();
+    return dir;
+}
